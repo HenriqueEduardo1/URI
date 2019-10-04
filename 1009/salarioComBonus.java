88f39706fb7fead844870fe0bda6617a0020e9ed
Henrique <32546360+HenriqueEduardo1@users.noreply.github.com>
2019-10-04 09:10:12 -0300

    Update salarioComBonus.java

--- 1009/salarioComBonus.java
+++ 1009/salarioComBonus.java
@@ -1,27 +1,13 @@
 import java.io.IOException;
-
 import java.util.Scanner; 
-
-public class Main {
-   
-    public static void main(String[] args) throws IOException {
- 
-        
-
+public class Main {   
+    public static void main(String[] args) throws IOException {        
         Scanner in = new Scanner(System.in);
-
-        
-        
-        String a = in.nextLine();
-        
-        double b = in.nextDouble();
-        
+       
+        String a = in.nextLine();      
+        double b = in.nextDouble();       
         double c = in.nextDouble();
-
-        
-        
-        System.out.format("TOTAL = R$ %.2f\n", (b + (c * 0.15)));
-  
+              
+        System.out.format("TOTAL = R$ %.2f\n", (b + (c * 0.15)));  
     }
-
 }
